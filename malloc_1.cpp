12e8e91c110c7c2b84b6b228c576b4b347edfe2a
Shahar Payevsky <shahar.payevsky@gmail.com>
2024-08-05 15:23:25 +0300

    add malloc_1.cpp and smalloc

---
+++ malloc_1.cpp
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+#define MAX_SIZE 100000000
+
+void* smalloc(size_t size) {
+    if(size == 0 || size > MAX_SIZE) {  //verify value of size is legal (0 < size < MAX_SIZE)
+        return NULL;
+    }
+    //TODO: verify size value is non-negative? referring the note in part 1
+    void* result = sbrk(size);
+    if(result == (void*)(-1)) {
+        return NULL;
+    }
+    return result;
+
+}
